ff8e053c21fd03c3b08bc7e9e23f0bbaaf982721
teeko-k <ahmed.taher.eng@gmail.com>
2020-11-27 11:35:53 +1100

    executing command line from node

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 const fs = require('fs')
 const chalk = require('chalk')
+const { exec } = require('child_process')
 
 const currentPath = process.cwd()
 
 const simply = function () {
   fs.writeFileSync(`${currentPath}/new-file.txt`, 'Hello from Simply!')
 }
+
+exec('ls -la', (error, stdout, stderr) => {
+  if (error) {
+    return console.log(chalk.bgRed(chalk.black(` ${error} `)))
+  }
+  if (stderr) {
+    return console.log(chalk.bgRed(chalk.black(` ${error} `)))
+  }
+
+  console.log(chalk.cyan(`output: ${stdout}`))
+})
 
 console.clear()
 console.log(chalk.bgCyanBright(chalk.black(` file created at ${currentPath} `)))
